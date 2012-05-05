7b2d3ea0bc577d089a65e6c9413b2cd335a39e6c
Joe Smith <yasumoto7@gmail.com>
2012-05-05 03:06:13 -0700

    Asynchronously download Flickr photos on a dispatch queue.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- JoeTopPlaces/PhotoViewController.m
+++ JoeTopPlaces/PhotoViewController.m
@@ -33,27 +33,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSURL *photoURL = [FlickrFetcher urlForPhoto:self.photo format:FlickrPhotoFormatLarge];
-    NSData *photo = [[NSData alloc] initWithContentsOfURL:photoURL];
-    UIImage *image = [UIImage imageWithData:photo];
-    self.imageView = [[UIImageView alloc] initWithImage:image];
     self.scrollView.minimumZoomScale = MINIMUM_SCROLL_SCALE;
     self.scrollView.maximumZoomScale = MAXIMUM_SCROLL_SCALE;
-    self.navigationItem.title = [self.photo objectForKey:FLICKR_PHOTO_TITLE];
+    
 }
 
 - (void) viewWillAppear:(BOOL)animated {
-    [self.scrollView addSubview:self.imageView];
-    self.scrollView.zoomScale = 1;
-    self.scrollView.contentSize = self.imageView.bounds.size;
-    CGFloat widthZoom = self.scrollView.bounds.size.width / self.imageView.image.size.width;
-    CGFloat heightZoom = self.scrollView.bounds.size.height / self.imageView.image.size.height;
-    if (widthZoom > heightZoom) {
-        self.scrollView.zoomScale = widthZoom;
-    }
-    else {
-        self.scrollView.zoomScale = heightZoom;
-    }
+    dispatch_queue_t downloadPhotoQueue = dispatch_queue_create("downlooad photo from flickr", NULL);
+    dispatch_async(downloadPhotoQueue, ^{
+        NSURL *photoURL = [FlickrFetcher urlForPhoto:self.photo format:FlickrPhotoFormatLarge];
+        NSLog(@"We are downloading a photo located at: %@", photoURL);
+        NSData *photo = [[NSData alloc] initWithContentsOfURL:photoURL];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage *image = [UIImage imageWithData:photo];
+            self.imageView = [[UIImageView alloc] initWithImage:image];
+            self.navigationItem.title = [self.photo objectForKey:FLICKR_PHOTO_TITLE];
+            [self.scrollView addSubview:self.imageView];
+            self.scrollView.zoomScale = 1;
+            self.scrollView.contentSize = self.imageView.bounds.size;
+            CGFloat widthZoom = self.scrollView.bounds.size.width / self.imageView.image.size.width;
+            CGFloat heightZoom = self.scrollView.bounds.size.height / self.imageView.image.size.height;
+            if (widthZoom > heightZoom) {
+                self.scrollView.zoomScale = widthZoom;
+            }
+            else {
+                self.scrollView.zoomScale = heightZoom;
+            }
+        });
+    });
+    dispatch_release(downloadPhotoQueue);
 }
 
 - (void) viewDidAppear:(BOOL)animated {
